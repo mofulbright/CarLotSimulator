fa44ad1a6bf261599de9ce6982951660e519d812
mofulbright <mofulbright02@gmail.com>
2022-03-31 13:43:06 -0300

    update

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,6 +18,41 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            CarLot carLot = new CarLot();
+
+            //Dot notation
+            var vw = new Car();
+            vw.Year = 2001;
+            vw.Make = "Volkswagen";
+            vw.Model = "Passat";
+            vw.EngineNoise = "Rattling";
+            vw.HonkNoise = "beep beep";
+            vw.IsDriveable = true;
+
+            carLot.ParkingLot.Add(vw);
+            
+
+            //Object Initializer syntax
+            var acura = new Car()
+            {
+                Make = "Acura",
+                Model = "TL",
+                Year = 2001,
+                IsDriveable = true,
+                EngineNoise = "purr",
+                HonkNoise = "honk honk"
+            };
+            carLot.ParkingLot.Add(acura);
+            
+
+            //Custom Constructor
+            var lincoln = new Car(2010, "Lincoln", "MKZ", "Grrrrr", "ROAR", true);
+            carLot.ParkingLot.Add(lincoln);
+
+            //Instantiate CarLot
+
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -28,6 +63,14 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            foreach(var car in carLot.ParkingLot)
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+                car.MakeEngineNoise();
+                car.MakeHonkNoise();
+                Console.WriteLine("------------------");
+
+            }
         }
     }
 }
